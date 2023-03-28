c42c3d4004cce6709fa22f21dc1228c9a211e10d
Moises Gualapuro <43993933+gualapuromoises@users.noreply.github.com>
2023-03-28 13:13:25 +0000

    Fue generado utn3.txt AQUI SI FUNCIONA EL CÓDIGO

--- utn.sh
+++ utn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tail -n +2 ./data/Buzzard2015_data.csv |cut -d "," -f 3| sort|uniq -c|sort -r > utn1.txt
+tail -n +2 ./data/Buzzard2015_data.csv |cut -d "," -f 3| sort|uniq -c|sort -r > utn3.txt
 
